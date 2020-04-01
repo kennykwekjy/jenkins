d2889ab5d424086c810266d266af102f06528498
Bang <hbtruong.2017@sis.smu.edu.sg>
2020-04-01 16:08:49 +0800

    update

--- test.py
+++ test.py
@@ -15,15 +15,16 @@ class TestHello(unittest.TestCase):
         self.assertEqual(rv.status, '200 OK')
 
     def test_hello_hello(self):
-            rv = self.app.get('/tbank/')
+        rv = self.app.get('/tbank/')
 
-            response = {
-                            "data": "tBank API is up and running", 
-                            "status": "failed"
-                        }
+        response = {
+                        "data": "tBank API is up and running", 
+                        "status": "success"
+                    }
 
-            self.assertEqual(rv.status, '200 OK')
-            self.assertEqual(json.loads(rv.data), response)
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(json.loads(rv.data), response)
+
     # def test_hello_name(self):
     #     name = 'Kenny'
     #     rv = self.app.get(f'/tbank/{name}')
