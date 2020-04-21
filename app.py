e30a36eef98b6835cca3b1a954712ae82dafc306
kenny kwek <kenny.kwek.2017@sis.smu.edu.sg>
2020-04-21 14:40:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello_world():
 def hello_user(username):
     return jsonify(
         status = "success",
-        data = "User " + username + " is currently active."
+        data = "User " + username + " is currently very dry & active."
     ), 200
 
   
